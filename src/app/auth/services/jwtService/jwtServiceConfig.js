e848c5517dcd2f4a8bdcc3fff9701bee4cdacb10
parth <parthsingh1910@gmail.com>
2023-02-13 21:53:35 +0530

    authentication and refresh token

---
+++ src/app/auth/services/jwtService/jwtServiceConfig.js
@@ -0,0 +1,9 @@
+const jwtServiceConfig = {
+  signIn: 'user/signin',
+  signUp: 'api/auth/sign-up',
+  accessToken: 'api/auth/access-token',
+  updateUser: 'api/auth/user/update',
+  verifyToken: 'user/verify-token',
+};
+
+export default jwtServiceConfig;
