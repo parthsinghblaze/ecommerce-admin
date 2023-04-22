9f7454a12ae35a4c5c9b379eb5bf0024ed639d60
parthsingh <parthsingh1910@gmail.com>
2023-04-22 13:11:51 +0530

    rollback the category list

--- src/app/main/Category/Category.js
+++ src/app/main/Category/Category.js
@@ -1,5 +1,5 @@
 import FusePageCarded from '@fuse/core/FusePageCarded';
-import { styled } from '@mui/material/styles';
+import useThemeMediaQuery from '@fuse/hooks/useThemeMediaQuery';
 import withReducer from 'app/store/withReducer';
 import CategoryHeader from './components/CategoryHeader';
 import CategoryTable from './components/CategoryTable';
@@ -7,31 +7,14 @@ import reducer from './store';
 
 function Category() {
 
-  const Root = styled(FusePageCarded)(({ theme }) => ({
-    '& .FusePageCarded-header': {
-      minHeight: 72,
-      height: 72,
-      alignItems: 'center',
-      [theme.breakpoints.up('sm')]: {
-        minHeight: 136,
-        height: 136,
-      },
-    },
-    '& .FusePageCarded-content': {
-      display: 'flex',
-    },
-    '& .FusePageCarded-contentCard': {
-      overflow: 'hidden',
-    },
-  }));
-
+  const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
 
   return (
-    <Root
+    <FusePageCarded
       header={<CategoryHeader />}
       content={<CategoryTable />}
       // scroll='content'
-      innerScroll
+      scroll={isMobile ? 'normal' : 'content'}
     />
   );
 }
